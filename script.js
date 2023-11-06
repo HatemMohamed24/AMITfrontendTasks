29a2d775284e248977a7b47bd9d36dddbb767334
HatemMohamed24 <hm2240257@gmail.com>
2023-11-06 15:35:18 +0300

    Task0

---
+++ script.js
@@ -0,0 +1,96 @@
+
+let $name = "HATEM"
+let $lastName ="Mohamed"
+
+console.log($lastName)
+
+console.log($name) 
+
+let PI = 3.14 
+console.log (PI)
+// comment 
+/*  
+
+multiline comment 
+
+*/ 
+//console.log (typeof "hello world")
+//console.log (typeof 123)
+//console.log (typeof false)
+//console.log (typeof true)
+//console.log (typeof null) 
+
+//et age = 12 
+//et firstName = "ali"
+//let is_active = true
+//let no_data = null 
+
+//onsole.log(typeof age)
+
+
+
+let now = 2023 
+let year = 1999 
+
+console.log(now - year)
+
+
+let a = 2 ,  b = 3 
+
+console.log (a**b)
+
+
+let firstName = "ali" , lastName = "hamada"
+
+console.log(firstName + " " + lastName)
+
+
+let d = 1 , f = 2 
+d = 5 
+f+=1 
+f*=2
+f /=2
+console.log (f)
+
+
+
+let k = 7 
+
+console.log ( k  > 5 )
+console.log ( k  < 5 )
+console.log ( k  >= 5 )
+console.log ( k  <= 5 ) 
+console.log ( k  == 5 ) 
+console.log ( k  != 5 ) 
+
+let greater_than =  k > 5 
+
+console.log (greater_than)
+console.log (typeof greater_than)
+
+
+console.log (1+3*4/5**2-7)
+//console.log (1+3*4/5**2-7)
+//5**2 = 25 
+//(1+3*4/25-7)
+// 3*4 = 12 
+// (1+12/25-7)
+// 12/25 = 0.48
+//(1+.48-7)
+
+
+let first_name = "ahmed"
+let age = 22 
+console.log (first_name + age)
+
+
+
+//let now = 2023 
+//let year = 1999 
+//let your year = 2000 
+
+
+
+
+let length = 22 , width = 2 
+console.log ("the area = " + length * width )
